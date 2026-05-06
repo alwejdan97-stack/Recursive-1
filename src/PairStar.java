f9c6590ddd0e2d8aa9423008a6e34fab517c1ac2
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:04:44 +0400

    add base case and recursive case

--- src/PairStar.java
+++ src/PairStar.java
@@ -1,3 +1,10 @@
 public class PairStar {
-    public String pairStar(String str){}
+    public String pairStar(String str){
+        if(str.length()==0 || str.length()==1){
+            return str;
+        }else if(str.charAt(0)==str.charAt(1)){
+            return str.charAt(0)+"*"+pairStar(str.substring(1));
+        }
+        return str.charAt(0)+pairStar(str.substring(1));
+    }
 }
