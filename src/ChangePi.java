feb6e64de6bfbd8cb75bdcf47f7f6e5e2a819114
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 16:24:02 +0400

    add base case and recursive case

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -1,3 +1,11 @@
 public class ChangePi {
-    public String changePi(String str){}
+    public String changePi(String str){
+        if(str.length()==0){
+            return "";
+        }
+        else if(str.startsWith("pi")){
+            return "3.14" + changePi(str.substring(2));
+        }
+        return str.charAt(0)+changePi(str.substring(1));
+    }
 }
