0fa5e0c4b2497c4acf7e4bf187772815e964fc86
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:12:27 +0400

    Add Task 7: Create a Recursive PairStar Program in Java

--- src/PairStar.java
+++ src/PairStar.java
@@ -1,8 +1,11 @@
 public class PairStar {
     public String pairStar(String str){
+        //base case
         if(str.length()==0 || str.length()==1){
             return str;
-        }else if(str.charAt(0)==str.charAt(1)){
+        }
+        //recursive case
+        else if(str.charAt(0)==str.charAt(1)){
             return str.charAt(0)+"*"+pairStar(str.substring(1));
         }
         return str.charAt(0)+pairStar(str.substring(1));
