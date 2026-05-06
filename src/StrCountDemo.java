47b649c6d3740e41d052db37298f90653cc19c4e
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:43:10 +0400

    Add Task 9: Create a Recursive StrCount Program in Java

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -1,7 +1,7 @@
 public class StrCountDemo {
     public static void main(String[] args){
-        StrCount strCount=new StrCount();
-        System.out.println(strCount.strCount("catcowcat", "cat"));
+        StrCount strCount=new StrCount();// create StrCount object
+        System.out.println(strCount.strCount("catcowcat", "cat"));// calling strCount method
         System.out.println(strCount.strCount("catcowcat", "cow"));
         System.out.println(strCount.strCount("catcowcat", "dog"));
     }
