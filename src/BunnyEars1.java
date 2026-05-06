98edbc32194f9913f0a56364324c83770bd7620c
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:57:00 +0400

    Add Task 10: Create a Recursive BunnyEars Program in Java

--- src/BunnyEars1.java
+++ src/BunnyEars1.java
@@ -1,8 +1,10 @@
 public class BunnyEars1 {
     public int bunnyEars(int n){
+        //base case
         if(n==0){
             return 0;
         }
+        //recursive case
         else{
             return 2 + bunnyEars(n - 1);
         }
