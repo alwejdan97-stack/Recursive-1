17a4bfd0a921bae697ce704558d4fa473b21b149
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 16:10:01 +0400

    Add Task 4: Create a Recursive CountX Program in Java

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -1,5 +1,9 @@
 public class CountXDemo {
     public static void main(String[] args){
-        CountX countx=new CountX();
+        CountX countx=new CountX(); //create CountX object
+        System.out.println(countx.countX("xxhixx")); //calling countX method
+        System.out.println(countx.countX("xhixhix"));
+        System.out.println("hi");
+        System.out.println("hix");
     }
 }
