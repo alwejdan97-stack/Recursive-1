e9166f46b53afec8f38e512b3685fba8e4f3474a
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:56:27 +0400

    Add Task 10: Create a Recursive BunnyEars Program in Java

--- src/BunnyEarsDemo1.java
+++ src/BunnyEarsDemo1.java
@@ -1,7 +1,7 @@
 public class BunnyEarsDemo1 {
     public static void main(String[] args){
-        BunnyEars1 bunnyEars1=new BunnyEars1();
-        System.out.println(bunnyEars1.bunnyEars(0) );
+        BunnyEars1 bunnyEars1=new BunnyEars1();// create BunnyEars1 object
+        System.out.println(bunnyEars1.bunnyEars(0) ); // calling BunnyEars1
         System.out.println(bunnyEars1.bunnyEars(1) );
         System.out.println(bunnyEars1.bunnyEars(2) );
     }
