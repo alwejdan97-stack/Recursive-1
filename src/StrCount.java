270260410c5c8fdc00a1b4595bcde81c8a421da7
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:47:29 +0400

    Add Task 9: Create a Recursive StrCount Program in Java

--- src/StrCount.java
+++ src/StrCount.java
@@ -1,9 +1,11 @@
 public class StrCount {
     public int strCount(String str, String sub){
+        // base case
         if(str.length()<sub.length()){
             return 0;
         }
-        else if(str.substring(sub.length()).equals(sub)){
+        //recursive case
+        else if(str.substring(0,sub.length()).equals(sub)){
             return 1 + strCount(str.substring(sub.length()), sub);
         }
         return strCount(str.substring(1), sub);
