6bb1e91abce5661c228f1552f0487f3d106c611a
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 15:25:34 +0400

    Task 1: Create a Recursive Factorial Program in Java

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,6 +1,10 @@
 public class Factorial {
-    public static void main(String[] args){}
     public static int  factorial(int n){
-
+        if(n==1){
+            return n;
+        }
+        else{
+            return  factorial(n-1)*n;
+        }
     }
 }
