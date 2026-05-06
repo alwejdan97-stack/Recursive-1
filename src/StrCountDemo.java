770430cb859505f906fbfa169ae7bcdd6227279e
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:42:12 +0400

    create StrCount inside StrCountDemo class

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -1,3 +1,8 @@
 public class StrCountDemo {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        StrCount strCount=new StrCount();
+        System.out.println(strCount.strCount("catcowcat", "cat"));
+        System.out.println(strCount.strCount("catcowcat", "cow"));
+        System.out.println(strCount.strCount("catcowcat", "dog"));
+    }
 }
