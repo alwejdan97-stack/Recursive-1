4b4ee9f4d9f7783ea2ac649c45f417176330a4f7
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 15:27:07 +0400

    Add Task 1: Create a Recursive Factorial Program in Java

--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -1,7 +1,7 @@
 public class FactorialDemo {
-    public static Factorial factorial=new Factorial();
+    public static Factorial factorial=new Factorial(); //create object for Factorial class
     public static void main(String[] args){
-        System.out.println(Factorial.factorial(1));
+        System.out.println(Factorial.factorial(1)); // calling factorial() from Factorial class
         System.out.println(Factorial.factorial(2));
         System.out.println(Factorial.factorial(3));
     }
