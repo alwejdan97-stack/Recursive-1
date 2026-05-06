f503a1df05a92d254e156969dbc83ef7a95107cf
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:31:32 +0400

    Task 8: Create a Recursive CountAbc Program in Java

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -2,7 +2,7 @@ public class CountAbcDemo {
     public static void main(String[] args){
         CountAbc countAbc=new CountAbc(); //create CountAbc object
         System.out.println(countAbc.countAbc("abc")); //calling countAbc method
-        System.out.println(countAbc.countAbc("abcxxabc"));
+        System.out.println(countAbc.countAbc("abcxxabc")); //
         System.out.println(countAbc.countAbc("abaxxaba"));
     }
 }
