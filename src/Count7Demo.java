ad63315af4bb699f57143babf1a495017bbdcf64
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 15:59:01 +0400

    Add Task 3: Create a Recursive Count7 Program in Java

--- src/Count7Demo.java
+++ src/Count7Demo.java
@@ -1,5 +1,8 @@
 public class Count7Demo {
     public static void main(String[] args){
-        Count7 count7=new Count7();
+        Count7 count7=new Count7();// create Count7 object
+        System.out.println(count7.count7(717));// calling count7 method
+        System.out.println(count7.count7(7));
+        System.out.println(count7.count7(123));
     }
 }
