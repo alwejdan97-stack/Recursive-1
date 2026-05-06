e40393ff29698023b3f348fdd175354ae3f9fd70
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 15:32:34 +0400

    add base case and recursive case

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -1,3 +1,12 @@
 public class BunnyEars {
-    public int bunnyEars2(int n){}
+    public int bunnyEars2(int n){
+        if(n==1){
+            return n;
+        }else{
+            if(n % 2 == 0){
+                return 3 + bunnyEars2(n - 1);
+            }
+            return 2 + bunnyEars2(n - 1);
+        }
+    }
 }
