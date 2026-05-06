8caabf63a383df093ba0c3a1034691fbea1c1987
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 16:45:41 +0400

    add base case and recursive case

--- src/Array11.java
+++ src/Array11.java
@@ -1,4 +1,10 @@
 public class Array11 {
-    public int array11(int[] nums, int index){}
-
+    public int array11(int[] nums, int index){
+        if(index == nums.length){
+            return 0;
+        }else if(nums[index] == 11){
+            return 1 + array11(nums, index + 1);
+        }
+        return array11(nums, index + 1);
+    }
 }
