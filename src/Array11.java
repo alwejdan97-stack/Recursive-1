2d4db89a56a250e1ee595e1446ae76274126f72f
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 16:55:56 +0400

    Add Task 6: Create a Recursive Array11 Program in Java

--- src/Array11.java
+++ src/Array11.java
@@ -1,8 +1,11 @@
 public class Array11 {
     public int array11(int[] nums, int index){
+        //base case
         if(index == nums.length){
             return 0;
-        }else if(nums[index] == 11){
+        }
+        //recursive case
+        else if(nums[index] == 11){
             return 1 + array11(nums, index + 1);
         }
         return array11(nums, index + 1);
