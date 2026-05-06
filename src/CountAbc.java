854839ce3d75e71cb4f586a7a2073b36ad5ae7eb
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:21:37 +0400

    add base case and recursive case

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -1,3 +1,10 @@
 public class CountAbc {
-    public int countAbc(String str){}
+    public int countAbc(String str){
+        if(str.length()==3){
+            return 0;
+        }else if(str.substring(0,4)=="abc"||str.substring(0,4)=="aba"){
+            return 1 + countAbc(str.substring(1));
+        }
+        return countAbc(str.substring(1));
+    }
 }
