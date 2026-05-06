aa7ffa03a1f971ce827d9a224b3d4852357daf6a
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-06 17:30:47 +0400

    public class CountAbc {

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -1,3 +1,8 @@
 public class CountAbcDemo {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        CountAbc countAbc=new CountAbc(); //create CountAbc object
+        System.out.println(countAbc.countAbc("abc")); //calling countAbc method
+        System.out.println(countAbc.countAbc("abcxxabc"));
+        System.out.println(countAbc.countAbc("abaxxaba"));
+    }
 }
