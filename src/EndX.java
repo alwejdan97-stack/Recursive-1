259fc295fbe02612143d063d4780f107954bb0ef
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:47:55 +0400

    add base case and recursive case

--- src/EndX.java
+++ src/EndX.java
@@ -1,3 +1,13 @@
 public class EndX {
-    public String endX(String str){}
+    public String endX(String str){
+        if(str.length()==0){
+            return "";
+        }
+        else{
+            if(str.charAt(0)=='x'){
+                return endX(str.substring(1))+1;
+            }
+                return str.charAt(0)+endX(str.substring(1));
+        }
+    }
 }
