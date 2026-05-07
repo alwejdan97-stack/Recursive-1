9d763c000124ad0678640cbcdcb6cadc4c996954
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 10:03:24 +0400

    Add Task 16: Create a Recursive Count11 Program in Java

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -1,5 +1,8 @@
 public class Count11Demo {
     public static void main(String[] args){
         Count11 count11=new Count11();
+        System.out.println(count11.count11("11abc11"));
+        System.out.println(count11.count11("abc11x11x11"));
+        System.out.println(count11.count11("111"));
     }
 }
