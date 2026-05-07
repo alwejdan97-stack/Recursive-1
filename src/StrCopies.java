586db8f0216962a5cce34e03006c1eee2a7cfc94
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 11:06:29 +0400

    Add Task 18: Create a Recursive StrCopies Program in Java

--- src/StrCopies.java
+++ src/StrCopies.java
@@ -1,11 +1,12 @@
 public class StrCopies {
     public Boolean strCopies(String str, String sub, int n){
+        //base case
         if(n==0){
             return true;
         }else if(str.length()<sub.length()){
             return false;
         }
-
+        //recursive case
         else if(str.substring(0,sub.length()).equals(sub)){
                 return strCopies(str.substring(1), sub, n - 1);
             }
