fd69b752f6f16dec69facfe35bf860bfc92408e4
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 08:34:36 +0400

    Add Task 11: Create a Recursive Triangle Blocks Program in Java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,8 +1,10 @@
 public class Triangle {
     public int triangle(int n){
+        //base case
         if(n==0){
             return 0;
         }
+        //recursive case
         else{
             return n + triangle(n - 1);
         }
