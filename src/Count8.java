efd9ad6eb29bee65f790ecde1532c69ac6ff07e2
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:03:39 +0400

    Add Task 12: Create a Recursive Count8 Program in Java

--- src/Count8.java
+++ src/Count8.java
@@ -2,9 +2,12 @@ public class Count8 {
     public int count8(int n) {
         //int last = n % 10;
         //int secondLast = (n / 10) % 10;
+        //base case
         if (n == 0) {
             return 0;
-        } else if(n % 10 == 8){
+        }
+        //recursive case
+        else if(n % 10 == 8){
             if( (n/10) % 10 == 8) {
                     return count8(n/10)+2;
                 }
