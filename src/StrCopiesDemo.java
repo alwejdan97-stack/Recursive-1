f10b5076a7163976dbdbc574cf3838768ab9fc09
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 11:05:06 +0400

    Add Task 18: Create a Recursive StrCopies Program in Java

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -1,5 +1,8 @@
 public class StrCopiesDemo {
     public static void main(String[] args){
         StrCopies strCopies=new StrCopies();
+        System.out.println(strCopies.strCopies("catcowcat", "cat", 2));
+        System.out.println(strCopies.strCopies("catcowcat", "cow", 2));
+        System.out.println(strCopies.strCopies("catcowcat", "cow", 1));
     }
 }
