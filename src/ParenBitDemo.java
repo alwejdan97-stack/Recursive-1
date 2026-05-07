31443b8fa9419db45f2fbf7eeab234b1b28aa6a7
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 10:27:39 +0400

    Add Task 17: Create a Recursive ParenBit Program in Java

--- src/ParenBitDemo.java
+++ src/ParenBitDemo.java
@@ -1,7 +1,7 @@
 public class ParenBitDemo {
     public static void main(String[] args){
-        ParenBit parenBit=new ParenBit();
-        System.out.println(parenBit.parenBit("xyz(abc)123"));
+        ParenBit parenBit=new ParenBit(); //create ParenBit object
+        System.out.println(parenBit.parenBit("xyz(abc)123")); //calling parenBit method
         System.out.println(parenBit.parenBit("x(hello)"));
         System.out.println(parenBit.parenBit("(xy)1"));
     }
