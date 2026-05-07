8ac5309cc4bf7068083997f8bf46fb580dc88d9b
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 10:12:22 +0400

    add base case and recursive case

--- src/Count11.java
+++ src/Count11.java
@@ -1,3 +1,11 @@
 public class Count11 {
-    public int count11(String str){}
+    public int count11(String str){
+        if(str.length()==0){
+            return 0;
+        }
+        else if(str.substring(0,2).equals("11")){
+            return 1+ count11(str.substring(2));
+        }
+        return count11(str.substring(1));
+    }
 }
