4c97fab9edcc6f35efe562ea20f5c57e116d6c07
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:31:54 +0400

    Add Task 14: Create a Recursive NoX Program in Java

--- src/NoXDemo.java
+++ src/NoXDemo.java
@@ -1,5 +1,8 @@
 public class NoXDemo {
     public static void main(String[] args){
-        NoX noX=new NoX();
+        NoX noX=new NoX(); // create NoX object
+        System.out.println(noX.noX("xaxb")); //calling noX method
+        System.out.println(noX.noX("abc"));
+        System.out.println(noX.noX("xx"));
     }
 }
