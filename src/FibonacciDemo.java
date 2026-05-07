898504f0c79c933c7e889d0b66274c6958abe470
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 11:13:53 +0400

    Add Task 19: Create a Recursive Fibonacci Program in Java

--- src/FibonacciDemo.java
+++ src/FibonacciDemo.java
@@ -1,7 +1,7 @@
 public class FibonacciDemo {
     public static void main(String[] args){
-        Fibonacci fibonacci=new Fibonacci();
-        System.out.println(fibonacci.fibonacci(0));
+        Fibonacci fibonacci=new Fibonacci(); //create Fibonacci object
+        System.out.println(fibonacci.fibonacci(0)); // calling fibonacci method
         System.out.println(fibonacci.fibonacci(1));
         System.out.println(fibonacci.fibonacci(2) );
     }
