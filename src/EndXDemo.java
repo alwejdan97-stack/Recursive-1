7b0c97d553b74c0c8f404b6ffdfbb64df2dc281e
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:52:54 +0400

    Add Task 15: Create a Recursive EndX Program in Java

--- src/EndXDemo.java
+++ src/EndXDemo.java
@@ -1,7 +1,7 @@
 public class EndXDemo {
     public static void main(String[] args){
-        EndX endX=new EndX();
-        System.out.println(endX.endX("xre"));
+        EndX endX=new EndX(); //create EndX object
+        System.out.println(endX.endX("xre")); //calling endX method
         System.out.println(endX.endX("xxhixx"));
         System.out.println(endX.endX("xhixhix"));
     }
