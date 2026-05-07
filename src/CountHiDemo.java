a7a14555da43987b25d02b52bfd17a04b6c765f0
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:17:21 +0400

    Add Task 13: Create a Recursive CountHi Program in Java

--- src/CountHiDemo.java
+++ src/CountHiDemo.java
@@ -1,7 +1,7 @@
 public class CountHiDemo {
     public static void main(String[] args){
         CountHi countHi=new CountHi(); //create CountHi object
-        System.out.println(countHi.countHi("xxhixx"));
+        System.out.println(countHi.countHi("xxhixx")); // calling countHi method
         System.out.println(countHi.countHi("xhixhix"));
         System.out.println(countHi.countHi("hi"));
     }
