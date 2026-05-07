d21d604ba04fb05adfe5763b5f7de72dc38f2ef8
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:12:35 +0400

    add base case and recursive case

--- src/CountHi.java
+++ src/CountHi.java
@@ -1,5 +1,11 @@
 public class CountHi {
     public int countHi(String str){
-
+        if(str.length()<2){
+            return 0;
+        }
+        else if(str.substring(0,2).equals("hi")){
+            return 1 + countHi(str.substring(2));
+        }
+        return countHi(str.substring(1));
     }
 }
