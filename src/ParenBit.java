da3b063ad7d22716af624a9c036e1a3fe99b7ee4
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 10:47:14 +0400

    add base case and recursive case

--- src/ParenBit.java
+++ src/ParenBit.java
@@ -1,5 +1,16 @@
 public class ParenBit {
     public String parenBit(String str){
-
+        if(str.length()<2 || !str.contains("(")){
+            return "";
+        }
+        else{
+            if(str.startsWith("(") && str.endsWith(")")){
+                return str;
+            }else if(str.charAt(0)!='('){
+                return parenBit(str.substring(1));
+            }
+                return parenBit(str.substring(0, str.length() - 1));
+        }
+        //return parenBit(str);
     }
 }
