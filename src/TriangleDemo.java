8427ffaede5283f91f55febfdfddd134840c8849
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 08:33:47 +0400

    Add Task 11: Create a Recursive Triangle Blocks Program in Java

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -1,5 +1,8 @@
 public class TriangleDemo {
     public static void main(String[] args){
-        Triangle triangle=new Triangle();
+        Triangle triangle=new Triangle(); // creating Triangle object
+        System.out.println(triangle.triangle(0)); //calling triangle method from object class
+        System.out.println(triangle.triangle(1));
+        System.out.println(triangle.triangle(2));
     }
 }
