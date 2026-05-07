0a58ceb7acd74682d4df4c1b6bc6920033a20b56
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:03:16 +0400

    add base case and recursive case

--- src/Count8.java
+++ src/Count8.java
@@ -1,16 +1,15 @@
 public class Count8 {
     public int count8(int n) {
-        int last = n % 10;
-        int secondLast = (n / 10) % 10;
+        //int last = n % 10;
+        //int secondLast = (n / 10) % 10;
         if (n == 0) {
             return 0;
-        } else {
-            if (last == 8 && secondLast == 8) {
-                return count8(2);
-            } else if (last == 8 && secondLast != 8) {
-                return count8(1);
+        } else if(n % 10 == 8){
+            if( (n/10) % 10 == 8) {
+                    return count8(n/10)+2;
+                }
+            return count8(n/10)+1;
             }
-                return count8(0);
-        }
+                return count8(n/10);
     }
 }
