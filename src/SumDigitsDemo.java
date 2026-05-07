a3f665e44ecce35116539d429aa037188260803f
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 11:21:07 +0400

    Add Task 20: Create a Recursive SumDigits Program in Java

--- src/SumDigitsDemo.java
+++ src/SumDigitsDemo.java
@@ -1,7 +1,7 @@
 public class SumDigitsDemo {
     public static void main(String[] args){
-        SumDigits sumDigits=new SumDigits();
-        System.out.println(sumDigits.sumDigits(126));
+        SumDigits sumDigits=new SumDigits(); //create SumDigits object
+        System.out.println(sumDigits.sumDigits(126));// calling sumDigits method
         System.out.println(sumDigits.sumDigits(49));
         System.out.println(sumDigits.sumDigits(12));
     }
