f6c2fc1c562e06197d2235c853c41b6c54578a03
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 11:21:37 +0400

    Add Task 20: Create a Recursive SumDigits Program in Java

--- src/SumDigits.java
+++ src/SumDigits.java
@@ -1,8 +1,10 @@
 public class SumDigits {
     public int sumDigits(int n){
+        //base case
         if(n==0) {
             return 0;
         }
+        //recursive base
         else {
             return n % 10 + sumDigits(n / 10);
         }
