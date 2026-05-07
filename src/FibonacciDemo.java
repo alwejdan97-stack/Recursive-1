809dd2d28dd43fd04d5cf200c866c18eaa6ccce0
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 11:11:40 +0400

    create Fibonacci object inside FibonacciDemo class

--- src/FibonacciDemo.java
+++ src/FibonacciDemo.java
@@ -1,5 +1,5 @@
 public class FibonacciDemo {
     public static void main(String[] args){
-        FibonacciDemo fibonacciDemo=new FibonacciDemo();
+        FibonacciDemo fibonacci=new FibonacciDemo();
     }
 }
