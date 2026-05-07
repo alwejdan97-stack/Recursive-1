a5dfbc75c2dbee857d15daa4a2bcf83c3c19dff9
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-07 09:41:01 +0400

    add base case and recursive case

--- src/NoX.java
+++ src/NoX.java
@@ -6,10 +6,6 @@ public class NoX {
         else{
             if(str.charAt(0)=='x'){
                 return noX(str.substring(1));
-            }else if(str.charAt(0)!='x'){
-                return "x"+noX(str.substring(1));
-            }else if(!(str.contains("x"))){
-                return noX(str.substring(1));
             }
         }
         return str.charAt(0)+noX(str.substring(1));
